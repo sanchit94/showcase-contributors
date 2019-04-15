988dc4b9d2ff67ee9b2ad34e3c016eb4e5d97352
Sanchit <sanchits174@gmail.com>
2019-04-15 18:04:44 +0530

    Added a fixed header bar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,33 +1,11 @@
 import React, { Component } from 'react';
-import { connect } from "react-redux";
 
-import { Header } from 'semantic-ui-react';
 
-class AppBar extends Component {
-    renderHeading = () => {
-        return this.props.boards[1].listIds.map(listId => {
-            return (<div className="list-responsive" key={listId} width={3}>
-                        <Header as='h3'>{this.props.lists[listId].name}</Header>
-                    </div>)
-        })
-    }
 
-    render() {
-        return(
-        <header className="app-bar mt-4 ml-4">
-            <div className="d-flex">
-                {!this.props.loading && this.renderHeading()}
-            </div>
-        </header>
-            
-        );
-    }
+function AppBar(props) {
+    return(
+        <div className="app-header">Nice to be here</div>
+    );
 }
 
-const mapDispatchToProps = store => ({
-    lists: store.lists,
-    boards: store.boards.boards,
-    loading: store.loading
-})
-
-export default connect(mapDispatchToProps, null)(AppBar);
+export default AppBar;

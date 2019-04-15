7413a4661198b2cbf8db6bc42adb2b2c1a7ad64e
Sanchit <sanchits174@gmail.com>
2019-04-15 06:59:03 +0530

    Composed Animated Accordion

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,74 +1,114 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
-import { Card, Image, Accordion } from 'semantic-ui-react';
+import { Card, Image, Icon } from 'semantic-ui-react';
+import { useSpring, animated } from 'react-spring';
+import styled from 'styled-components';
 
-import { domain } from '../constants'
+import { useMeasure, usePrevious } from './helpers/react-spring-helpers';
+import { domain } from '../constants';
 
-class CardInfo extends Component {
 
-    getIndex = () => {
-        const { id } = this.props;
-        const index = this.props.cards.findIndex(
+const Content = styled(animated.div)`
+  will-change: transform, height;
+  overflow: hidden;
+`
+  
+
+function CardInfo(props) {
+    const [open, setValue] = useState(0);
+    const previous = usePrevious(open);
+    const [bind, { height: viewHeight }] = useMeasure();
+    const { height, transform } = useSpring({
+        height: open ? `${viewHeight}px` : '0px',
+        transform: open ? `rotate(-180deg)`: `rotate(0deg)`,
+        from: {
+            height: '0px',
+            transform: 'rotate(0deg)'
+        }});
+
+    const getIndex = () => {
+        const { id } = props;
+        const index = props.cards.findIndex(
             card => card.id === id
         );
         return index;
 
     }
 
-    renderDate = () => {
-        const index = this.getIndex();
-        let { timeStamp } = this.props.cards[index];
-        timeStamp = new Date(timeStamp);
-        const now = new Date();
-        if (timeStamp.toDateString() === now.toDateString()) {
-            return "Today";
-        }
-        return timeStamp.toDateString().slice(0,9);
-
-    }
-
-    renderCard = () => {
-        const index = this.getIndex();
-        const imageSource = `${domain}/uploads/${this.props.cards[index].cardImage}`;
-
-        const panel = [{
-            key: index,
-            title: {
-                content: (
-                    <div>
-                <Card.Header className="card-header">{this.props.cards[index].heading}</Card.Header>
-                {/* <Card.Meta className="mt-2">
-                    <span className='date'>{this.renderDate()}</span>
-                </Card.Meta> */}
-                </div>),
-                icon: ''
-            },
-            content: {
-                content: <Card.Description>{this.props.cards[index].content}</Card.Description>
-            }
-        }];
-
-        return(
-            <Card className="raised card-norad mb-2" fluid>
-                <div></div> {/* For avoiding wierd card css from semantic-ui */}
-                {this.props.cards[index].cardImage && <Image src={imageSource} alt="Couldn't load image" />}
-                <div className={`card__labels__${this.props.cards[index].priority || 2}`}>
-                </div>
-                <Card.Content>
-                    <Accordion className="mt--2" panels={panel} fluid />
-                </Card.Content>
-            </Card>
+    const accordionContent = () => {
+        const index = getIndex();
+        return (
+            <Content style={{height: open && previous === open ? 'auto' : height}}>
+            <animated.div className="anim-from-top hide-overflow" {...bind}>
+            {props.cards[index].content}
+            </animated.div>
+            </Content>
         );
-
     }
-    render() {
+
+    // const renderDate = () => {
+    //     const index = this.getIndex();
+    //     let { timeStamp } = this.props.cards[index];
+    //     timeStamp = new Date(timeStamp);
+    //     const now = new Date();
+    //     if (timeStamp.toDateString() === now.toDateString()) {
+    //         return "Today";
+    //     }
+    //     return timeStamp.toDateString().slice(0,9);
+    // }
+
+    const expandModal = () => {
+        console.log(open);
+        return setValue(state => {
+            return state ? 0 : 1
+        });
+    }
+
+    
+
+    const renderCard = () => {
+        const index = getIndex();
+        const imageSource = `${domain}/uploads/${props.cards[index].cardImage}`;
         return(
             <div>
-            {!this.props.loading && this.renderCard()}
+            <Card className="raised card-norad mb-2" fluid>
+                <div></div> {/* For avoiding wierd card css from semantic-ui */}
+                {props.cards[index].cardImage && <Image src={imageSource} alt="Couldn't load image" />}
+                <div className={`card__labels__${props.cards[index].priority || 2}`}>
+                </div>
+                <Card.Content>
+                <Card.Header className="card-header" onClick={expandModal}>
+                    {props.cards[index].heading}
+                    { 
+                        props.cards[index] && props.cards[index].content && 
+                    <animated.div className="float-right" style={{transform}}>
+                    <Icon name="angle down"></Icon>     
+                    </animated.div>
+                    }
+                </Card.Header>
+                </Card.Content>
+                <Card.Content>
+                    <div>
+                { props.cards[index] && props.cards[index].content && accordionContent() }
+                </div>
+                
+                    </Card.Content>
+            </Card>
+            {/* <Card style={{height, transform}} className="raised card-norad mb-2" fluid>
+            <Card.Content style={{height, transform}}>
+            
+            </Card.Content>
+            </Card> */}
             </div>
         );
     }
+        
+    return(
+        <div>
+        {!props.loading && renderCard()}
+        </div>
+    );
 }
 
 const mapDispatchToProps = store => ({

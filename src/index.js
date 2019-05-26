c804971524e78b897b645ac33ae7eed212d973be
Sanchit <sanchits174@gmail.com>
2019-05-27 02:52:15 +0530

    Customizing semantic-ui styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "semantic-ui-css/semantic.min.css";
+import 'semantic-ui-less/semantic.less';
 
 import Root from './Root';
 import { HashRouter } from "react-router-dom";

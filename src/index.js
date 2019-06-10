2279a91326f07335bdf279ac0421e37548ea90e1
Sanchit <sanchits174@gmail.com>
2019-06-10 23:31:24 +0530

    Firebase initialized in index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import 'semantic-ui-less/semantic.less';
 
 import Root from './Root';
 import { HashRouter } from "react-router-dom";
+import * as firebase from 'firebase';
 
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAj3JWUu8PeYi3OWuWqt2ghT7XzN0WqQvk",
+    authDomain: "roadmap-714c8.firebaseapp.com",
+    databaseURL: "https://roadmap-714c8.firebaseio.com",
+    projectId: "roadmap-714c8",
+    storageBucket: "roadmap-714c8.appspot.com",
+    messagingSenderId: "408935502660",
+    appId: "1:408935502660:web:d6ea7551b3fd0d08"
+  };
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
     <Root>

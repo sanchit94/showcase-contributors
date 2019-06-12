65a8e148b8785e8174988d4d770385ead2e7a299
Sanchit <sanchits174@gmail.com>
2019-06-12 13:01:58 +0530

    Reidrect with popup instead of redirect

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../actions/user';
+import * as firebase from 'firebase';
 
 import { Image, Button } from 'semantic-ui-react';
 import logo from '../images/logo.png';
@@ -29,6 +30,15 @@ const style = {
 
 function AppBar(props) {
 
+    const logout = () => {
+        firebase.auth().signOut().then(function() {
+            props.logout();
+            // Sign-out successful.
+          }).catch(function(error) {
+            // An error happened.
+          });
+    }
+
     
     // const getName = () => {
     //     if(localStorage.getItem('username')) {
@@ -42,7 +52,7 @@ function AppBar(props) {
             <Link to="/"><Image src={logo} style={style.image}/></Link>
             <h2 className="visible-md" style={style.centerText}>Product Roadmap</h2>
             <SuggestButton />
-            {props.isLoggedIn && <Button onClick={() => props.logout()} className="greenish">Sign out</Button>}
+            {props.isLoggedIn && <Button onClick={logout} className="greenish">Sign out</Button>}
             {!props.isLoggedIn && <LoginButton />}
             {!props.isLoggedIn && <SignUpButton />}
         </div>
--- src/components/SocialIconGroup.js
+++ src/components/SocialIconGroup.js
@@ -3,25 +3,18 @@ import { Icon } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import * as firebase from 'firebase';
 
-import { loginFB, loginTwitter, login, loginAsync } from '../actions/user';
+import { loginFB, loginTwitter, login, loginAsync, logout } from '../actions/user';
 
 class SocialIconGroup extends Component {
-    
+        
     glogin = () => {
-        const that = this;
-        console.log('meme');
-        let provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(provider).then(function(result) {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then((result) => {
             console.log("Does it go here");
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-            console.log(token);
-            // The signed-in user info.
             var user = result.user;
-            console.log(user);
-            that.props.loginAsync(user.email)
+            this.props.loginAsync(user.email)
             .then(res => {
-                that.props.login(res.data);
+                this.props.login(res.data);
             })
             // ...
           }).catch(function(error) {
@@ -37,21 +30,69 @@ class SocialIconGroup extends Component {
           });
     }
 
+    fbLogin = () => {
+        var provider = new firebase.auth.FacebookAuthProvider();
+        firebase.auth().signInWithPopup(provider).then((result) => {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            this.props.loginAsync(user.email)
+            .then(res => {
+                this.props.login(res.data);
+            });
+            // ...
+          }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+          });
+    }
+
+    twitterLogin = () => {
+        var provider = new firebase.auth.TwitterAuthProvider();
+        firebase.auth().signInWithPopup(provider).then((result) => {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            this.props.loginAsync(user.email)
+            .then(res => {
+                this.props.login(res.data);
+            });
+            // ...
+          }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+          });
+    }
+
     render() {
         return(
             <div className="block-centered text-center">
-                <Icon circular inverted size="large" name="facebook f" />
-                <Icon circular inverted size="large" name="twitter" />
+                <Icon onClick={this.fbLogin} circular inverted size="large" name="facebook f" />
+                <Icon onClick={this.twitterLogin} circular inverted size="large" name="twitter" />
                 <Icon onClick={this.glogin} circular inverted size="large" name="google" />                
             </div>
         );
     }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = () => {
     return {
     }
 }
 
-export default connect(mapStateToProps, { loginFB, loginAsync, login, loginTwitter })(SocialIconGroup);
+export default connect(mapStateToProps, { loginFB, loginAsync, login, logout, loginTwitter })(SocialIconGroup);
 
